4e0d9bc25b9d5d7a29c80d856aba64fb7f024813
Sandip <sandipdeshmukh77@gmail.com>
2025-10-08 21:14:48 +0530

    add YouTube link to Footer component with SVG icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,6 +44,22 @@ const Footer: React.FC = () => {
                         <Mail size={18} />
                         <span>Email</span>
                     </a>
+                    {/* YouTube */}
+                    <a
+                        href="https://www.youtube.com/@neuralabz"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center space-x-1 hover:text-white transition-colors"
+                    >
+                        {/* YouTube SVG icon */}
+                        <svg width={18} height={18} viewBox="0 0 24 24" fill="none" aria-hidden="true">
+                            <path
+                                d="M23.498 6.186a2.994 2.994 0 0 0-2.112-2.112C19.413 3.5 12 3.5 12 3.5s-7.413 0-9.386.574A2.994 2.994 0 0 0 .502 6.186C0 8.16 0 12 0 12s0 3.84.502 5.814a2.994 2.994 0 0 0 2.112 2.112C4.587 20.5 12 20.5 12 20.5s7.413 0 9.386-.574a2.994 2.994 0 0 0 2.112-2.112C24 15.84 24 12 24 12s0-3.84-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"
+                                fill="currentColor"
+                            />
+                        </svg>
+                        <span>YouTube</span>
+                    </a>
                 </div>
             </div>
         </footer>
